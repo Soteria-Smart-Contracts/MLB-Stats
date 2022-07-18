c52748e80a0b65602889fd8b521312d41357d960
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-18 15:42:16 -0400

    July 18, 2022 3:42 PM

--- mlb.js
+++ mlb.js
@@ -13,7 +13,7 @@ async function GetPlayerOBP(MLBID){
 async function GetGameIDs(){
     let currentDate = new Date;
     let formatteddate = `${currentDate.getFullYear()}-${ReturnMonthString(currentDate.getMonth() + 1)}-21`;
-
+    
     return formatteddate;
 }
 
