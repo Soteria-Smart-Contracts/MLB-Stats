0d9c05430ac6a54a15a3078678e967c6c5a4ecc4
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-18 16:47:58 -0400

    July 18, 2022 4:47 PM

--- ScrapeMLBcom.js
+++ ScrapeMLBcom.js
@@ -1,3 +1,11 @@
 
 
-async function GetGameDayLinks()
+async function GetGameDayLinks(){
+    let currentDate = new Date;
+    let formatteddate = `${currentDate.getFullYear()}-${ReturnMonthString(currentDate.getMonth() + 1)}-22`;
+    console.log(formatteddate);
+    GamedayRequest = await (await fetch('https://api.sportsdata.io/v3/mlb/scores/json/GamesByDate/' + formatteddate + APIkey)).json();
+    let GameIDs = ParseGameFileForIDs(GamedayRequest);
+
+    return GameIDs;
+}
