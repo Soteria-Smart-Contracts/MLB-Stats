8d9ae9ff1657498942745ad77a66b168ba533732
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-18 13:03:50 -0400

    July 18, 2022 1:03 PM

--- mlb.js
+++ mlb.js
@@ -1,5 +1,5 @@
 const APIprefix = 'http://lookup-service-prod.mlb.com';
 
 async function GetPlayerOBP(MLBID){
-    PlayerHittingStats = await fetch(`${APIprefix}/json/named.s_player_hitting_tm.bam?game_type='R'&season='2022'&player_id='${MLBID}'`)
+    PlayerHittingStats = await fetch(`${APIprefix}/json/named.sport_hitting_tm.bam?league_list_id='mlb'&game_type='R'&season={season}&player_id={player_id}`)
 }
