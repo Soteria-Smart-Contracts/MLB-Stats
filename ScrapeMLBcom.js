33305d7f16071096954fb0f2331ed736869ab059
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-18 19:15:59 -0400

    July 18, 2022 7:15 PM

--- ScrapeMLBcom.js
+++ ScrapeMLBcom.js
@@ -6,10 +6,10 @@ async function GetGameDayLinks(){
 
     let currentDate = new Date;
     let month = '0' + (currentDate.getMonth() + 1);
-    let formatteddate = `${currentDate.getFullYear()}-${month}-27`;
+    let formatteddate = `${currentDate.getFullYear()}-${month}-21`;
     
     const parser = new DOMParser();
-    const parsedSite = parser.parseFromString(await (await fetch('https://www.mlb.com/scores/' + formatteddate)).text(), "text/html");
+    const parsedSite = parser.parseFromString(await (await fetch('https://cors-anywhere.herokuapp.com/https://www.mlb.com/scores/' + formatteddate)).text(), "text/html");
 
     LinkBoxes = parsedSite.getElementsByClassName('linkstyle__AnchorElement-sc-1rt6me7-0 lcFuuA getProductButtons__ButtonLink-sc-bgnczd-1 elIcfn trk-preview');
     GameDayLinks = await ParseBoxesForLinks(LinkBoxes);
