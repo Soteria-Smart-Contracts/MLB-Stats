cf4e139d43b4ebd9f9b9f4250d95aece3308c968
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-19 11:15:32 -0400

    July 19, 2022 11:15 AM

--- mlb.js
+++ mlb.js
@@ -15,44 +15,8 @@ async function GetPlayerOBP(MLBID){
 
 
 
+
+
 //BaseLevel Functions
 
 
-function ReturnMonthString(month){
-    if(month == 1){
-        return "JAN";
-    }
-    if(month == 2){
-        return "FEB";
-    }
-    if(month == 3){
-        return "MAR";
-    }
-    if(month == 4){
-        return "APR";
-    }
-    if(month == 5){
-        return "MAY";
-    }
-    if(month == 6){
-        return "JUN";
-    }
-    if(month == 7){
-        return "JUL";
-    }
-    if(month == 8){
-        return "AUG";
-    }
-    if(month == 9){
-        return "SEP";
-    }
-    if(month == 10){
-        return "OCT";
-    }
-    if(month == 11){
-        return "NOV";
-    }
-    if(month == 12){
-        return "DEC";
-    }
-}
