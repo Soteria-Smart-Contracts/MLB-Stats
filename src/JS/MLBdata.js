402a883c14b4f392817b1d45dee984d7982bddfd
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-19 14:56:30 -0400

    July 19, 2022 2:56 PM

--- src/JS/MLBdata.js
+++ src/JS/MLBdata.js
@@ -36,7 +36,7 @@ async function GetPitcherFIP(MLBID){
 
 
 async function CalculateFIP(HR,BB,HBP,SO,IP){
-    let FIP = ((13 * HR) + (3 * (BB + HBP)) - (2 * SO) / IP);
+    let FIP = ((13 * HR) + (3 * (BB + HBP)) - (2 * SO) / IP).toFixed(3);
     return FIP;
 }
 
