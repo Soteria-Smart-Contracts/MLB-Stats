48da6797f928e6786639db5e2aacba25b89832c4
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-20 13:44:15 -0400

    July 20, 2022 1:44 PM

--- src/JS/Website.js
+++ src/JS/Website.js
@@ -4,6 +4,9 @@ function DropDown(id) {
     if (Dropdown1 == false) {
         document.getElementById(id).style.display = "block";
         Dropdown1 = true;
+    } else {
+        document.getElementById(id).style.display = "none";
+        Dropdown1 = false;
     }
     console.log(Dropdown1);
 }
