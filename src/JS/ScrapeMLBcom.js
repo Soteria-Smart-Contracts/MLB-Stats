800f3cafef0ea3f21728f78cc7fe40de9508ecdb
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-20 19:00:57 -0400

    July 20, 2022 7:00 PM

--- src/JS/ScrapeMLBcom.js
+++ src/JS/ScrapeMLBcom.js
@@ -41,4 +41,20 @@ function ParseBoxesForLinks(LinkBoxes){
     return Links;
 }
 
-//
+// Deprecated functions
+
+async function GetGameDayLinks(){
+
+    let currentDate = new Date;
+    let month = '0' + (currentDate.getMonth() + 1);
+    let formatteddate = `${currentDate.getFullYear()}-${month}-21`;
+    
+    const parser = new DOMParser();
+    const parsedSite = parser.parseFromString(await (await fetch('https://cors-anywhere.herokuapp.com/https://www.mlb.com/scores/' + formatteddate)).text(), "text/html");
+
+    LinkBoxes = parsedSite.getElementsByClassName('linkstyle__AnchorElement-sc-1rt6me7-0 lcFuuA getProductButtons__ButtonLink-sc-bgnczd-1 elIcfn trk-preview');
+    GameDayLinks = await ParseBoxesForLinks(LinkBoxes);
+
+    console.log(GameDayLinks);
+    return GameDayLinks;
+}
