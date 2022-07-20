a29efcbeab069418540fae8e3c5edece023025d4
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-20 18:38:19 -0400

    July 20, 2022 6:38 PM

--- src/JS/ScrapeMLBcom.js
+++ src/JS/ScrapeMLBcom.js
@@ -29,9 +29,7 @@ async function GetLineups(){
     
     Boxes = parsedSite.getElementsByClassName('lineup__box');
 
-    
-
-    return Boxes[0].getElementsByClassName('lineup__abbr');
+    ;
 }
 
 
