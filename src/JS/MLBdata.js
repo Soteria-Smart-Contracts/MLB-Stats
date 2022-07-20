b6368e43943347abcb83bcf994954ba88781f49f
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-20 19:28:22 -0400

    July 20, 2022 7:28 PM

--- src/JS/MLBdata.js
+++ src/JS/MLBdata.js
@@ -44,7 +44,7 @@ GetPlayerID(FullName){
     let StatRequest = await fetch(`${APIprefix}/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='${FullName}'`)
     let PlayerInfo = await StatRequest.json();
 
-    Player
+    let PlayerID = PlayerInfo.search_player_all.queryResults.row.player_id;
 }
 
 
