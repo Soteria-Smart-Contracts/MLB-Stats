732414b9346d9d0cbbaa3fdd30b128cbfe30addf
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-20 20:49:26 -0400

    July 20, 2022 8:49 PM

--- src/JS/ScrapeMLBcom.js
+++ src/JS/ScrapeMLBcom.js
@@ -58,7 +58,7 @@ function ParseBoxesForLinks(LinkBoxes){
 }
 
 async function GetLineupOrders(BoxesRW){
-    let LineupBoxes = BoxesRW
+    let LineupBoxes = BoxesRW.getElementsByClassName('lineup__box');
 
     index = 0;
     total = BoxesMLB.length;
