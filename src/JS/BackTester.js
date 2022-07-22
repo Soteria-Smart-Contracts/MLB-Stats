10559732e78b651b80980ec6b7e60b8ba82a1ab7
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-22 14:46:22 -0400

    July 22, 2022 2:46 PM

--- src/JS/BackTester.js
+++ src/JS/BackTester.js
@@ -2,6 +2,6 @@ let SeasonData = fetch("src/GL2021.xlsx");
 
 function ReadJson(){
     let json = fetch("https://github.com/Soteria-Smart-Contracts/MLB-Stats/blob/master/src/SeasonData2021.json");
-    return json;
+    return json[1];
 }
 
