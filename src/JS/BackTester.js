f649589c7d22a0bf31e6b1065c39384071f3740f
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-22 18:36:00 -0400

    July 22, 2022 6:36 PM

--- src/JS/BackTester.js
+++ src/JS/BackTester.js
@@ -13,6 +13,16 @@ async function BackTestSeason(){
     let TwoEight = [0,0,0];
     let TwoNine = [0,0,0];
     let ThreeZero = [0,0,0];
+    let ThreeOne = [0,0,0];
+    let ThreeTwo = [0,0,0];
+    let ThreeThree = [0,0,0];
+    let ThreeFour = [0,0,0];
+    let ThreeFive = [0,0,0];
+    let ThreeSix = [0,0,0];
+    let ThreeSeven = [0,0,0];
+    let ThreeEight = [0,0,0];
+    let ThreeNine = [0,0,0];
+    let FourZero = [0,0,0];
     
 }
 
