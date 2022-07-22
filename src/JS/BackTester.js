1d7453f8048237c3bbec35b3e28980be6869ed83
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-22 14:56:10 -0400

    July 22, 2022 2:56 PM

--- src/JS/BackTester.js
+++ src/JS/BackTester.js
@@ -1,7 +1,7 @@
 
 
 async function ReadJson(){
-    let request = await fetch("src/SeasonData.");
+    let request = await fetch("src/SeasonData2021.json");
     let json= await request.json();
     return json[1];
 }
