948ef131d9032d86abd2e7d5a11ac39b055cd85c
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-22 18:54:32 -0400

    July 22, 2022 6:54 PM

--- src/JS/BackTester.js
+++ src/JS/BackTester.js
@@ -41,8 +41,7 @@ async function BackTestSeason(){
         PitcherOneFIP = GetPitcherFIPfromName(json[i][i][0]);
         } 
         catch(err){errors = errors + 1; console.log(errors)}
-
-        PitcherTwoFIP
+        
         Over = json[i][i][3];
         console.log(PitcherOneFIP);
         console.log(PitcherTwoFIP);
