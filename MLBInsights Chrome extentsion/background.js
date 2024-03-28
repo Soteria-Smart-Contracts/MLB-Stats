19caeb3f7ab496d2244b5d06fa58f4136a15a2a5
Darius Tanz <scratchticketseb@gmail.com>
2024-03-28 17:07:00 +0000

    Mar 28, 2024, 1:07 PM

--- MLBInsights Chrome extentsion/background.js
+++ MLBInsights Chrome extentsion/background.js
@@ -2,15 +2,11 @@
 
 // Listen for tab change events
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url.startsWith('https://www.mlb.com/player/')) {
-      chrome.tabs.sendMessage(tabId, { action: 'logPlayerName' });
-    }
+
   });
   
   // Listen for the extension icon click event
   chrome.action.onClicked.addListener((tab) => {
-    // Execute JavaScript code here when the extension icon is clicked
-    // For example, you can open a new tab with a specific URL
-    chrome.tabs.create({ url: 'https://www.example.com' });
+    alert('Extension icon clicked!');
   });
   
