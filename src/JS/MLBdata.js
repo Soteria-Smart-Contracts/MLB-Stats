df0c2bfc045bdffa17ca5e9eace0bf17bce9d40c
Darius Tanz <scratchticketseb@gmail.com>
2024-03-28 16:07:39 +0000

    Mar 28, 2024, 12:07 PM

--- src/JS/MLBdata.js
+++ src/JS/MLBdata.js
@@ -1,43 +1,5 @@
-const APIprefix = 'http://lookup-service-prod.mlb.com';
-const APIkey = '?key=36e1a1ee5dcd46d994b4dd2bd86080a1';
-let PlayerHittingStats;
-let GamedayRequest;
-
-let HR;
-let BB;
-let HBP;
-let SO;
-let IP;
 
 
-async function GetBatterOBP(MLBID){
-    let StatRequest = await fetch(`${APIprefix}/json/named.sport_hitting_tm.bam?league_list_id='mlb'&game_type='R'&season='2022'&player_id=${MLBID}`)
-    let PlayerHittingStats = await StatRequest.json();
-    let PlayerOBP = PlayerHittingStats.sport_hitting_tm.queryResults.row.obp;
-    console.log(PlayerOBP);
-}
-
-async function GetPitcherFIP(MLBID){
-    let StatRequest = await fetch(`${APIprefix}/json/named.sport_pitching_tm.bam?league_list_id='mlb'&game_type='R'&season='2022'&player_id=${MLBID}`)
-    let PlayerPitchingStats = await StatRequest.json();
-    
-    HR = parseFloat(PlayerPitchingStats.sport_pitching_tm.queryResults.row.hr);
-    BB = parseFloat(PlayerPitchingStats.sport_pitching_tm.queryResults.row.bb);
-    HBP = parseFloat(PlayerPitchingStats.sport_pitching_tm.queryResults.row.hb);
-    SO = parseFloat(PlayerPitchingStats.sport_pitching_tm.queryResults.row.so);
-    IP = parseFloat(PlayerPitchingStats.sport_pitching_tm.queryResults.row.ip);
-
-    let FIP = await (((13 * HR) + (3 * (BB + HBP)) - (2 * SO)) / IP + 3.214).toFixed(3);
-    console.log(FIP);
-
-    return(FIP);
-}
-
-async function GetPitcherFIPfromName(FullName){
-    let PlayerID = await GetPlayerID(FullName);
-    let FIP = await GetPitcherFIP(PlayerID);
-    return FIP;
-}
 
 function calculateFIP(playerName) {
     // Split the CSV data by lines to get individual player records
