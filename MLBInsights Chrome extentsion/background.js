0c1cb2a1180bb74b606bc259e0cf4a7dc05f0471
Darius Tanz <scratchticketseb@gmail.com>
2024-03-28 16:47:48 +0000

    Mar 28, 2024, 12:47 PM

--- MLBInsights Chrome extentsion/background.js
+++ MLBInsights Chrome extentsion/background.js
@@ -1,5 +1,5 @@
 // background.js
-//check if the url has https://www.mlb.com/player/ in it
+//check if the url has https://www.mlb.com/player/ in it using a real 
 // Listen for tab change events
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.url.includes('https://www.mlb.com/player/')) {
