315adeb138683241123d890f77d40a9a4b159e2e
Darius Tanz <scratchticketseb@gmail.com>
2024-03-28 17:29:42 +0000

    Mar 28, 2024, 1:29 PM

--- MLBInsights Chrome extentsion/content.js
+++ MLBInsights Chrome extentsion/content.js
@@ -1,6 +1,6 @@
 //in class with only 1 element, use querySelector instead of querySelectorAll for player-header--vitals-name to get the player name
 //alert with the player name formatted
-let PlayerInfoFor
+let PlayerInfoFormattedForAlert =
 
 
 
