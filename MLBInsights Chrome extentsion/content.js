976624772d889c3593867058154a232670615c86
Darius Tanz <scratchticketseb@gmail.com>
2024-03-28 16:51:52 +0000

    Mar 28, 2024, 12:51 PM

--- MLBInsights Chrome extentsion/content.js
+++ MLBInsights Chrome extentsion/content.js
@@ -3,7 +3,7 @@
 // Listen for changes in the URL
 chrome.runtime.onMessage.addListener((message) => {
     if (message.action === 'logPlayerName') {
-      const playerNameElement = document.querySelector('.player-header__title h1');
+      const playerNameElement = document.querySelector('player-header--vitals-name');
       if (playerNameElement) {
         const playerName = playerNameElement.textContent.trim();
         console.log('Player Name:', playerName);
