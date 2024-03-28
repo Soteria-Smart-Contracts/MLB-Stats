4f4db9749612936aad77eed08314525c55ef173e
Darius Tanz <scratchticketseb@gmail.com>
2024-03-28 17:33:34 +0000

    Mar 28, 2024, 1:33 PM

--- MLBInsights Chrome extentsion/content.js
+++ MLBInsights Chrome extentsion/content.js
@@ -1,11 +1,11 @@
 //in class with only 1 element, use querySelector instead of querySelectorAll for player-header--vitals-name to get the player name
 //alert with the player name formatted
-let Go = async function() {
+
     let playerName = document.querySelector('.player-header--vitals-name').textContent;
     let PlayerInfo = GetPitcherInfo(playerName);
     FormattedAlertWithInfo = `Player Name: ${PlayerInfo.Name}\nAge: ${PlayerInfo.Age}\nTeam: ${PlayerInfo.Team}\nRecord: ${PlayerInfo.Record}\nFIP: ${PlayerInfo.FIP}\nERA: ${PlayerInfo.ERA}\nHR/9IP: ${PlayerInfo["HR/9IP"]}\nIP: ${PlayerInfo.IP}\nHR: ${PlayerInfo.HR}`;
-    alert(`Player Name: ${playerName}`);
-}
+alert(`Player Name: ${playerName}`);
+
 
 
 
