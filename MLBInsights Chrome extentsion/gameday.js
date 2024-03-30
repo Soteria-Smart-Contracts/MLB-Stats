22020047e2425da510357e9f94b20b59eacd4d37
Darius Tanz <scratchticketseb@gmail.com>
2024-03-30 15:21:50 +0000

    Mar 30, 2024, 11:21 AM

--- MLBInsights Chrome extentsion/gameday.js
+++ MLBInsights Chrome extentsion/gameday.js
@@ -1,18 +1,4 @@
 
-//if the word preview is in the url, then we are on the preview page so alert the user
-if(window.location.href.indexOf("preview") > -1) {
-    //get the 3 letter team codes for each team using the url and matching its contents to the teamcodes object (example link: https://www.mlb.com/gameday/brewers-vs-mets/2024/03/30/745850/preview)
-    let team1 = teamcodes[window.location.href.split("/")[4].split("-")[0]];
-    let team2 = teamcodes[window.location.href.split("/")[4].split("-")[1]];
-    alert("You are on the preview page for the " + team1 + " vs " + team2 + " game.");
-    //call the function to analyze the head to head home runs
-    //analyzeHeadToHeadHomeRuns(team1, team2);
-}
-
-async function Go(){
-    //get the 
-}
-
 let is_split = false;
 
 // New function for head-to-head analysis
