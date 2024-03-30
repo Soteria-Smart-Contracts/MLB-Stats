8307270bfd577caae565049946d4cc34b7e13c0d
Darius Tanz <scratchticketseb@gmail.com>
2024-03-30 15:00:00 +0000

    Mar 30, 2024, 11:00 AM

--- MLBInsights Chrome extentsion/gameday.js
+++ MLBInsights Chrome extentsion/gameday.js
@@ -1,3 +1,6 @@
 alert("gameday.js loaded");
 
 //if the word preview is in the url, then we are on the preview page so alert the user
+if(window.location.href.indexOf("preview") > -1) {
+    alert("You are on the preview page");
+}
