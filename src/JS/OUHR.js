192dee1de1e069eb47a1d2aa1942cdc22ab79e48
Darius Tanz <scratchticketseb@gmail.com>
2024-03-30 14:23:09 +0000

    Mar 30, 2024, 10:23 AM

--- src/JS/OUHR.js
+++ src/JS/OUHR.js
@@ -13,7 +13,7 @@ async function analyzeHomeRuns(teamToIndex) {
     // Loop through each game in the data
     for (const game of data) {
         console.log(game);
-      if (game[3] === teamToIndex || game[2] === teamToIndex) {
+      if (game[3] === teamToIndex || game[5] === teamToIndex) {
         totalHR += game[8]; // Assuming home runs are at index 8
         if (game[0] === teamToIndex) {
           totalHRF += game[5]; // Assuming home runs for at index 5
