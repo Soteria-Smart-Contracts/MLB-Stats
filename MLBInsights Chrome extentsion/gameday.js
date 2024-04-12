bad48ff4c7c8bf9127a17321ea3bdce033d85eee
Darius Tanz <scratchticketseb@gmail.com>
2024-04-12 15:09:39 +0000

    Apr 12, 2024, 11:09 AM

--- MLBInsights Chrome extentsion/gameday.js
+++ MLBInsights Chrome extentsion/gameday.js
@@ -132,8 +132,8 @@ async function analyzeHeadToHeadHomeRuns(team1, team2) {
         <p>Implied Odds for Under 2.5 HR: ${decimalOddsUnder}x</p>
         <p>Implied Odds for Under 1.5 HR: ${decimalOdds1p5Under}x</p>
     `;
-    const firstDiv = document.getElementsByTagName('head')[0];
-    head.insertBefore(infoBox, firstDiv);
+    const reactFooter = document.getElementById('react-footer');
+    reactFooter.parentNode.insertBefore(infoBox, reactFooter.nextSibling);
 
 }
 
