c87df04ead6ee286a2fe36aa89ecf1bfb275d79b
Darius Tanz <scratchticketseb@gmail.com>
2024-04-12 15:19:19 +0000

    Apr 12, 2024, 11:19 AM

--- MLBInsights Chrome extentsion/content.js
+++ MLBInsights Chrome extentsion/content.js
@@ -6,7 +6,6 @@ async function Go(){
     let playerName = document.querySelector('.player-header--vitals-name').textContent;
     let PlayerInfo = await GetPitcherInfo(playerName);
     FormattedAlertWithInfo = `Player Name: ${PlayerInfo.Name}\nAge: ${PlayerInfo.Age}\nTeam: ${PlayerInfo.Team}\nRecord: ${PlayerInfo.Record}\nFIP: ${PlayerInfo.FIP}\nERA: ${PlayerInfo.ERA}\nHR/9IP: ${PlayerInfo["HR/9IP"]}\nIP: ${PlayerInfo.IP}\nHR: ${PlayerInfo.HR}`;
-    alert(FormattedAlertWithInfo);
 }
 
 
